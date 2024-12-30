089c08c1de50b48c2f02c66266043170014983a7
William <wilguidro@gmail.com>
2024-12-31 00:51:35 +0100

    rightly documented

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,15 +3,19 @@
 This module contains a function `canUnlockAll` that determines whether all
 boxes in a list of lists can be unlocked.
 """
+
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
 
     Args:
-        boxes (list of lists): A list where each index contains a list of keys for other boxes.
+        boxes (list of lists): A list where each index contains a list of keys
+                               for other boxes.
 
     Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
+        bool: True if all boxes can be unlocked,
+              False otherwise.
     """
     n = len(boxes)  # Total number of boxes
     unlocked = [False] * n  # Track unlocked boxes
