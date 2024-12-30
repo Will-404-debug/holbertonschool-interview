cf62b00fd25c67fb391e0fb0e004ed1a5d4997b3
William <wilguidro@gmail.com>
2024-12-30 23:38:10 +0100

    Implement lockboxes challenge

---
+++ 0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Args:
+        boxes (list of lists): A list where each index contains a list of keys for other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    n = len(boxes)  # Total number of boxes
+    unlocked = [False] * n  # Track unlocked boxes
+    unlocked[0] = True  # The first box is initially unlocked
+    keys = set(boxes[0])  # Keys available from the first box
+
+    # Process until no new boxes can be unlocked
+    while keys:
+        new_key = keys.pop()  # Take a key from the set
+
+        if new_key < n and not unlocked[new_key]:
+            unlocked[new_key] = True  # Unlock the box
+            keys.update(boxes[new_key])  # Add new keys from the unlocked box
+
+    return all(unlocked)  # Check if all boxes are unlocked
