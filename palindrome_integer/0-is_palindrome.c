f4e4737b202c56911674b3a7cad81220ecb6473b
William <wilguidro@gmail.com>
2025-02-26 16:21:26 +0100

    Created the function that checks for palindrome

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Checks if an unsigned long integer is a palindrome
+ * @n: The number to check
+ *
+ * Return: 1 if n is a palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+    unsigned long reversed = 0, original = n, remainder;
+
+    while (n > 0)
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    return (original == reversed);
+}
