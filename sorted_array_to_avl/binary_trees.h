903bc684e9bd5570eb350c8876a569d536964bd2
William <wilguidro@gmail.com>
2025-04-03 18:22:48 +0200

    Create sorted array to avl - Update again

--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -20,5 +20,6 @@ typedef struct binary_tree_s avl_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 void binary_tree_delete(binary_tree_t *tree);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */
