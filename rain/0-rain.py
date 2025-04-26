347fcd2e3ea096ecdd3baea0148f201f512f5a07
William <wilguidro@gmail.com>
2025-04-26 23:17:48 +0200

    Correct indentation

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -13,12 +13,12 @@ def rain(walls):
     # Fill left_max array
     left_max[0] = walls[0]
     for i in range(1, n):
-    left_max[i] = max(left_max[i - 1], walls[i])
+        left_max[i] = max(left_max[i - 1], walls[i])
 
     # Fill right_max array
     right_max[n - 1] = walls[n - 1]
     for i in range(n - 2, -1, -1):
-    right_max[i] = max(right_max[i + 1], walls[i])
+        right_max[i] = max(right_max[i + 1], walls[i])
 
     # Calculate water trapped
     for i in range(n):
