4a2bc0578397fbcb15010775450439dde1a003c3
William <wilguidro@gmail.com>
2025-04-30 13:27:49 +0200

    Changed method

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -5,7 +5,5 @@
 
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
-void sift_down(int *array, size_t size, size_t i, size_t *swaps);
-void heapify(int *array, size_t size);
 
 #endif
