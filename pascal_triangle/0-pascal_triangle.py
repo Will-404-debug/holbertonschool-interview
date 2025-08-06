67e78074ff9f4455da66badce0fbba90eaffae13
William <wilguidro@gmail.com>
2025-08-06 13:54:01 +0200

    Pascal's triangle implementation and doc

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Defines function that returns a list of integers
+representing the pascal's triangle of n
+'''
+
+
+def pascal_triangle(n):
+    '''
+    Generates Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows in Pascal's triangle to generate.
+
+    Returns:
+        List[List[int]]: list of lists of ints representing Pascal's triangle.
+                         Each inner list represents a row of the triangle.
+
+    Example:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+    '''
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
